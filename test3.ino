75926f139993c9e9d84c95cad30aae91f7fda21f
Víctor Barceló <vbarcelo@gmail.com>
2015-01-01 20:16:44 +0000

    update

--- test3.ino
+++ test3.ino
@@ -13,12 +13,21 @@ const int SENSORPIN = 0;
 void setup(){
     Serial.begin(9600);
     Wire.begin(SLAVE_ADDRESS);
+    Wire.onReceive(receiveData); //opcional
     Wire.onRequest(sendData);
     Serial.println("Ready!");
 }
 
 void loop(){
     delay(100);
+}
+
+void receiveData(int byteCount){
+  	int cmd = Wire.read();
+  	if(cmd == 0)
+  	{
+    	//código
+  	}
 }
 
 void sendData(){
